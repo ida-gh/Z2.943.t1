38533798d61665338441417045f08bf1912a5362
Mohsen Odaressi <modarresi@levrek1.yonetim>
2023-09-17 10:04:05 +0300

    add

---
+++ LCNum6.pl
@@ -0,0 +1,7 @@
+set xrange[-0.5:0.5]
+set yrange[0:0.5]
+set xtics 0.100000
+set ytics 0.100000
+set grid
+set nokey
+plot "LCNum6.dat" index 0 w p ps 4.0 pt 7 lt 3 title "-1","LCNum6.dat" index 1 w p ps 4.0 pt 7 lt 1 title "+1"
